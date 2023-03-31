45cff2bf600976bd6ec8b3c209b1f33a4aeb88d7
AnkoorNagargoje <ankoornagargoje@gmail.com>
2023-03-31 16:11:31 +0530

    3rd commit

--- Formee/settings.py
+++ Formee/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cqnrsr(ii-n6+(t0$9plx(g5^#7x%fk1*d6^sc=ez^6&9pn0-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
