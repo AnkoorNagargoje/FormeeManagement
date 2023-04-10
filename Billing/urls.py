cf834e9be4b46c8ba563dda33524f0cab77d8932
AnkoorNagargoje <ankoornagargoje@gmail.com>
2023-04-10 17:58:15 +0530

    Edit and delete Order item

--- Billing/urls.py
+++ Billing/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('billing/add_customer/', views.add_new_customer, name='add_new_customer'),
     path('billing/<int:customer_id>/orders/', views.order_list, name='order_list'),
     path('billing/<int:customer_id>/orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('billing/<int:customer_id>/orders/<int:order_id>/<int:order_item_id>/edit/', views.order_item_edit, name='order_item_edit'),
+    path('billing/<int:customer_id>/orders/<int:order_id>/<int:order_item_id>/delete/', views.order_item_delete, name='order_item_delete'),
     path('billing/<int:order_id>/invoice/', views.generate_invoice),
     path('invoice/<int:order_id>/', views.invoice),
 ]
--- Billing/views.py
+++ Billing/views.py
@@ -6,6 +6,8 @@ from Stock.models import Quantity
 from django.http import HttpResponse
 from xhtml2pdf import pisa
 from num2words import num2words
+from django.template.loader import get_template
+from django.contrib import messages
 
 
 @login_required
@@ -73,7 +75,49 @@ def order_detail(request, customer_id, order_id):
     return render(request, 'order_detail.html', {'form': form, 'customer': customer, 'order': order, 'order_items': order_items})
 
 
-from django.template.loader import get_template
+def order_item_edit(request, customer_id, order_id, order_item_id):
+    customer = get_object_or_404(Customer, id=customer_id)
+    order = get_object_or_404(Order, id=order_id, customer=customer)
+    order_item = get_object_or_404(OrderItem, id=order_item_id, order=order)
+    old_quantity = order_item.quantity
+    form = OrderItemForm(request.POST or None, instance=order_item)
+    if form.is_valid():
+        new_order_item = form.save(commit=False)
+        product = order_item.product
+        if new_order_item.quantity > old_quantity:
+            diff = new_order_item.quantity - old_quantity
+            product.stock -= diff
+            order.order_total += order_item.product.price * diff
+        else:
+            diff = old_quantity - new_order_item.quantity
+            product.stock += diff
+            order.order_total -= order_item.product.price * diff
+        product.save()
+        new_order_item.save()
+        order.save()
+        messages.success(request, product.stock)
+        return redirect('order_detail', customer_id=customer.id, order_id=order.id)
+    return render(request, 'order_item_edit.html', {'form': form, 'customer': customer, 'order': order, 'order_item': order_item})
+
+
+def order_item_delete(request, customer_id, order_id, order_item_id):
+    customer = get_object_or_404(Customer, id=customer_id)
+    order = get_object_or_404(Order, id=order_id, customer=customer)
+    order_item = get_object_or_404(OrderItem, id=order_item_id, order=order)
+    product = order_item.product
+
+    # Add the quantity back to the product stock
+    product.stock += order_item.quantity
+    product.save()
+
+    # Subtract the order item total from the order total
+    order.order_total -= order_item.product.price * order_item.quantity
+    order.save()
+
+    # Delete the order item
+    order_item.delete()
+
+    return redirect('order_detail', customer_id=customer.id, order_id=order.id)
 
 
 def generate_invoice(request, order_id):
@@ -112,4 +156,4 @@ def invoice(request, order_id):
     total_amount = order.order_total
 
     # Render the HTML template
-    return render(request, 'invoice.html', {'order': order, 'customer': customer, 'order_items': order_items, 'total_amount': total_amount})
+    return render(request, 'invoice_view.html', {'order': order, 'customer': customer, 'order_items': order_items, 'total_amount': total_amount})
